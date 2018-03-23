e816ba9eeb07814b3a82fef0c9d0ffa07abbd6e3
Nicolas <nicolas@brauner.de>
2018-03-23 02:36:33 +0100

    venv und co

--- static/javascripts/ideas/controllers/ideasindex.controller.js
+++ static/javascripts/ideas/controllers/ideasindex.controller.js
@@ -15,6 +15,23 @@
             var vm = this;
             var canDelete = true;
 
+            $scope.search = {
+                not_feasible: false,
+                implemented: false
+            };
+            $scope.search.version = {};
+            $scope.search.version.label = "";
+            $scope.search.request_text = "";
+
+            $scope.customFilter = function (idea) {
+                alert(idea.manageable);
+                if (idea.manageable && idea.manageable !== $scope.search.not_feasible) {
+                    return idea;
+                } else if (!idea.manageable && idea.manageable === $scope.search.not_feasible) {
+                    return idea;
+                }
+            };
+
             $scope.isAuthenticated = Authentication.isAuthenticated();
             // console.error("ideas " + vm.isAuthenticated || false);
             $scope.ideas = [];
@@ -55,7 +72,7 @@
                 // Snackbar.show("Post deleted");
 
                 function deleteSuccessFn(data, status, headers, config) {
-                    Snackbar.show("Post deleted");
+                    // Snackbar.show("Post deleted");
                     $mdToast.show(
                         $mdToast.simple()
                             .textContent("Post deleted")
@@ -65,10 +82,10 @@
                 }
 
                 function deleteErrorFn(data, status, headers, config) {
-                    Snackbar.error(data.error);
+                    // Snackbar.error(data.error);
                     $mdToast.show(
                         $mdToast.simple()
-                            .textContent("data.error")
+                            .textContent(data.error)
                             .hideDelay(2000)
                     );
                     console.error(data.error);
@@ -113,6 +130,24 @@
                 $scope.currentPage = 1; //reset to first page
             }
 
+            $scope.versionFilter = function (label) {
+                $scope.search.version.label = label;
+            }
+
+
+            $scope.clickManageable = function () {
+                if ($scope.search.manageable == false) {
+                    $scope.search.manageable == true;
+                } else {
+                    $scope.search.manageable == false;
+                }
+                alert($scope.search.manageable);
+            }
+
+            $scope.filterReset = function () {
+                $scope.search = {};
+            }
+
             function activate() {
                 Ideas.all().then(ideasSuccessFn, ideasErrorFn);
 
@@ -126,17 +161,21 @@
 
                 function ideasSuccessFn(data, status, headers, config) {
                     $scope.ideas = data.data;
-                    $scope.viewby = 5;
                     $scope.totalItems = $scope.ideas.length;
                     $scope.currentPage = 1;
-                    $scope.itemsPerPage = $scope.viewby;
+                    $scope.itemsPerPage = 5;
                     $scope.maxSize = 5; //Number of pager buttons to show
                     // console.error("data " + data.data);
 
                 }
 
                 function ideasErrorFn(data, status, headers, config) {
-                    Snackbar.error(data.error);
+                    // Snackbar.error(data.error);
+                    $mdToast.show(
+                        $mdToast.simple()
+                            .textContent(data.error)
+                            .hideDelay(2000)
+                    );
                     console.error(data.error);
                 }
             }
@@ -147,8 +186,8 @@
                 function historySuccessFn(data, status, headers, config) {
                     $scope.versions = data.data;
                     $scope.newestVersion = $scope.versions[0];
-                    console.error("data " + data.data);
-                    console.error("v  " + $scope.newestVersion.label);
+                    // console.error("data " + data.data);
+                    // console.error("v  " + $scope.newestVersion.label);
 
                 }
 
