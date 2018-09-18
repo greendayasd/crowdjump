5056008a67c1af548a06867d05eb1fc357b937a0
Nicolas <nicolas@brauner.de>
2018-09-18 20:38:16 +0200

    mail functions

--- Crowdjump/mailFunctions.py
+++ Crowdjump/mailFunctions.py
@@ -9,11 +9,11 @@ def mail_new_version(request):
     if username != 'admin':
         return 'Wrong User'
 
-    version = 'version 0.06'
+    version = 'version 0.07'
     subject = 'Crowdjump ' + version + ' is available!'
     message1 = 'Hello '
-    message2 = ',<br>check out ' + version + ' of Crowdjump, the newest features are '
-    feature = "a bigger level with coins, which reduce your final time! There are also 3 eastereggs hidden in the level, can you find them all?"
+    message2 = ',<br>check out ' + version + ' of Crowdjump, the newest feature is '
+    feature = "a character selection! You can also upload your own character if you like!"
     message3 = '<br>To play the game and vote for your own ideas, visit  '
     html_content = '<a href="https://www.crowdjump.win">Crowdjump.win :)</a>'
     unsubscribe = '<br><br><a href="https://www.crowdjump.win/unsubscribe">Click here if you dont want to get this newsletter anymore</a>'
