496300e95ed8a8644e91f13ed4ec3cf40e4a2b86
Nicolas <nicolas@brauner.de>
2018-09-29 19:07:40 +0200

    030 mails

--- Crowdjump/mailFunctions.py
+++ Crowdjump/mailFunctions.py
@@ -1,5 +1,15 @@
 from django.core.mail import send_mail, send_mass_mail, EmailMultiAlternatives
 from authentication import models
+import os
+from email.mime.image import MIMEImage
+
+
+def logo_data(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+    logo = MIMEImage(data)
+    logo.add_header('Content-ID', '<logo>')
+    return logo
 
 
 def mail_new_version(request):
@@ -9,11 +19,18 @@ def mail_new_version(request):
     if username != 'admin':
         return 'Wrong User'
 
-    version = 'version 0.24'
-    subject = 'More lives? You get them in Crowdjump ' + version + '!'
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    folder_path = os.path.join(BASE_DIR, 'Screenshots')
+
+    screenshot_path = os.path.join(folder_path, '030.JPG')
+
+
+
+    version = 'version 0.30'
+    subject = 'Crowdjump got a new look!'
     message1 = 'Hello '
-    message2 = ',<br>check out ' + version + ' of Crowdjump, the newest features are '
-    feature = "animated coins as well as more lives! Completing the later levels should be a lot easier now. Also, you get an extra live for every 30 coins you collected!"
+    message2 = ',<br>check out the new features of Crowdjump ' + version + '! There are '
+    feature = "new deco elements, an explanation of all items and it's day or night in the game, depending on when you play!"
     message3 = '<br>To play the game, vote for ideas or submit your own, visit  '
     html_content = '<a href="https://www.crowdjump.win">Crowdjump.win :)</a>'
     unsubscribe = '<br><br><a href="https://www.crowdjump.win/unsubscribe">Click here if you dont want to get this newsletter anymore</a>'
@@ -27,11 +44,13 @@ def mail_new_version(request):
         final_message = message1 + user.username + message2 + feature + message3 + html_content + unsubscribe
         msg = EmailMultiAlternatives(subject, '', fromMail, [user.email])
         msg.attach_alternative(final_message, "text/html")
+        msg.attach(logo_data(screenshot_path))
         msg.send()
 
-    # final_message = message1 + 'admin' + message2 + feature + message3 + html_content
+    # final_message = message1 + 'admin' + message2 + feature + message3 + html_content + unsubscribe
     # msg = EmailMultiAlternatives(subject, '', fromMail, ['freshkd2@web.de'])
     # msg.attach_alternative(final_message, "text/html")
+    # msg.attach(logo_data(screenshot_path))
     # msg.send()
 
     return 'Mail send'
